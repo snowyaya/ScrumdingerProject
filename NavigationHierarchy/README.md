7ccd220d115a751e63c91fe1fef39e0bee7fa7cf
Yaya Liang <yayingliang@gmail.com>
2022-06-08 10:14:12 -0400

    Initial commit

---
+++ NavigationHierarchy/README.md
@@ -0,0 +1,5 @@
+# Creating a Navigation Hierarchy
+
+## Starting Project
+
+Use this project to code along with [Creating a Navigation Hierarchy](https://developer.apple.com/tutorials/app-dev-training/creating-a-navigation-hierarchy).
---
+++ NavigationHierarchy/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,18 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+*/
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
+        }
+    }
+}
+
+
