216aa29b57e781fd302fb8d9bbd3ccd447818027
Yaya Liang <yayingliang@gmail.com>
2022-06-14 09:02:59 -0400

    Add speech to text

---
+++ TranscribingSpeechToText/Scrumdinger/HistoryView.swift
@@ -0,0 +1,48 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by yayingliang on 6/14/22.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        // Add a navigation title to the scroll view that shows history.date with a .date style.
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var history: History {
+        History(attendees: [DailyScrum.Attendee(name: "Jon"), DailyScrum.Attendee(name: "Darla"), DailyScrum.Attendee(name: "Luis")], lengthInMinutes: 10, transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+    }
+    
+    static var previews: some View {
+        HistoryView(history: history)
+    }
+}
